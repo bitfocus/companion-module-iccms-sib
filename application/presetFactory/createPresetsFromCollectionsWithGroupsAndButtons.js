fc76140c4638a35056d910a38ab888d10b27a7da
Dmitri Peredera <Muhomorik@users.noreply.github.com>
2025-11-28 09:11:37 +0100

    Draft, new qb presets.

--- application/presetFactory/createPresetsFromCollectionsWithGroupsAndButtons.js
+++ application/presetFactory/createPresetsFromCollectionsWithGroupsAndButtons.js
@@ -2,34 +2,52 @@ import { createPresetFromButton } from './createPresetFromButton.js'
 
 /**
  * Create presets from collection with groups and buttons.
- * Don't create presets for collections without buttons.
+ * All presets are grouped under "QuickButtons/{collectionId}_{collectionName}/{groupName}".
+ * Spaces in names are replaced with underscores.
+ * Adds a header for each group.
  *
- * NOTE: only one level of nesting is possible.
  * @param {apiQuickButtonCollectionWithGroupsAndButtons[]} collections
  * @param {SibIcons} sibIcons
  * @returns {*} presets for setPresetDefinitions, <a href="https://github.com/bitfocus/companion-module-base/wiki/Presets">Presets</a>
  */
 export function createPresetsFromCollectionsWithGroupsAndButtons(collections, sibIcons) {
-	if (!Array.isArray(collections) || !collections) {
-		return []
-	}
-	const presets = {}
+  if (!Array.isArray(collections) || !collections) {
+    return {}
+  }
+  const presets = {}
 
-	// Collections (can only have one parent category, use separator.)
-	collections.forEach((qbCollection) => {
-		// Groups
-		qbCollection.Groups.forEach((qbGroup) => {
-			const category = '[' + qbGroup.ButtonText + '] ' + qbGroup.ButtonText
+  // Helper to sanitize names (replace spaces with underscores)
+  function sanitizeName(name) {
+    return (name || '').replace(/\s+/g, '_')
+  }
 
-			// Buttons in group.
-			qbGroup.Buttons.forEach((qb) => {
-				const presetQb = createPresetFromButton(category, qb, sibIcons)
-				if (presetQb != null) {
-					presets[`preset_qb_${qb.Id}`] = presetQb
-				}
-			})
-		})
-	})
+  collections.forEach((qbCollection) => {
+    const collectionName = qbCollection.CollectionName || 'Unnamed_Collection'
+    const collectionId = qbCollection.Id || 'noid'
+    const sanitizedCollectionName = sanitizeName(collectionName)
+    qbCollection.Groups.forEach((qbGroup) => {
+      const groupName = qbGroup.ButtonText || 'Unnamed_Group'
+      const sanitizedGroupName = sanitizeName(groupName)
+      const category = `QuickButtons/${sanitizedCollectionName}/${sanitizedGroupName}`
 
-	return presets
+      // Header for the group
+      const headerId = `header_${collectionId}_${qbGroup.Id || 'noid'}`
+      presets[headerId] = {
+        type: 'text',
+        category,
+        name: groupName,
+        text: 'Move this button to the canvas to activate',
+      }
+
+      // Buttons in group
+      qbGroup.Buttons.forEach((qb) => {
+        const presetQb = createPresetFromButton(category, qb, sibIcons)
+        if (presetQb != null) {
+          presets[`preset_qb_${qb.Id}`] = presetQb
+        }
+      })
+    })
+  })
+
+  return presets
 }
