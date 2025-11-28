9a9fa184c4b9c7971627c8d5db96d0536cbd8397
Dmitri Peredera <Muhomorik@users.noreply.github.com>
2025-11-28 16:36:56 +0100

    Update qb collection presets generation.

--- application/presetFactory/createPresetsFromCollectionsWithGroupsAndButtons.js
+++ application/presetFactory/createPresetsFromCollectionsWithGroupsAndButtons.js
@@ -1,5 +1,8 @@
 import { createPresetFromButton } from './createPresetFromButton.js'
 import { combineRgb } from '@companion-module/base'
+import { parseBgColorToPresetBgColor } from './parseBgColorToPresetBgColor.js'
+import { getForegroundColorFromBackgroundColor } from './getForegroundColorFromBackgroundColor.js'
+import { logger } from '../../logger.js'
 
 /**
  * Create presets from collection with groups and buttons.
@@ -27,42 +30,72 @@ export function createPresetsFromCollectionsWithGroupsAndButtons(collections, si
     const collectionId = qbCollection.Id || 'noid'
     const sanitizedCollectionName = sanitizeName(collectionName)
     
+    // Special case: if collection name is "QuickButtons", don't duplicate it
+    const collectionCategory = sanitizedCollectionName === 'QuickButtons' 
+      ? 'QuickButtons'
+      : `QuickButtons/${sanitizedCollectionName}`
+    
+    // Parse background color for collection header
+    let collectionBgClr = parseBgColorToPresetBgColor(qbCollection.BackgroundColorHex)
+    let collectionFgColor = getForegroundColorFromBackgroundColor(collectionBgClr)
+    
+    // Create collection header
+    const collectionHeaderId = `collection_header_${collectionId}`
+    presets[collectionHeaderId] = {
+      type: 'text',
+      category: collectionCategory,
+      name: collectionName,
+      text: collectionName,
+      style: {
+        text: collectionName,
+        size: 'auto',
+        color: collectionFgColor,
+        bgcolor: collectionBgClr,
+        pngalignment: 'center:center',
+      }
+    }
+    
+    // Add icon if available
+    if (sibIcons.hasIcon(qbCollection.IconId)) {
+      presets[collectionHeaderId].style.png64 = sibIcons.getIconPngBase64(qbCollection.IconId)
+    } else {
+      logger.debug('Preset collection header. Missing icon: %s', qbCollection.IconId)
+    }
+    
     qbCollection.Groups.forEach((qbGroup) => {
       const groupName = qbGroup.ButtonText || 'Unnamed_Group'
-      const sanitizedGroupName = sanitizeName(groupName)
       const groupId = qbGroup.Id || 'noid'
       
-      // Create group button under QuickButtons/CollectionName
-      const groupCategory = `QuickButtons/${sanitizedCollectionName}`
-      presets[`group_${collectionId}_${groupId}`] = {
-        type: 'button',
-        category: groupCategory,
-        name: groupName,
-        style: {
-          text: groupName,
-          size: 'auto',
-          color: combineRgb(255, 255, 255),
-          bgcolor: combineRgb(0, 0, 0),
-        },
-        steps: [{ down: [], up: [] }],
-        feedbacks: [],
-      }
+      // Parse background color for group header
+      let bgClrInt = parseBgColorToPresetBgColor(qbGroup.BackgroundColorHex)
+      let fgColor = getForegroundColorFromBackgroundColor(bgClrInt)
       
-      // Create buttons under group
-      const buttonCategory = `${groupCategory}/${sanitizedGroupName}`
-
-      // Header for the group
+      // Create group header in the collection category
       const headerId = `header_${collectionId}_${groupId}`
       presets[headerId] = {
         type: 'text',
-        category: buttonCategory,
+        category: collectionCategory,
         name: groupName,
-        text: 'Move this button to the canvas to activate',
+        text: groupName,
+        style: {
+          text: groupName,
+          size: 'auto',
+          color: fgColor,
+          bgcolor: bgClrInt,
+          pngalignment: 'center:center',
+        }
+      }
+      
+      // Add icon if available
+      if (sibIcons.hasIcon(qbGroup.IconId)) {
+        presets[headerId].style.png64 = sibIcons.getIconPngBase64(qbGroup.IconId)
+      } else {
+        logger.debug('Preset header. Missing icon: %s', qbGroup.IconId)
       }
 
-      // Individual buttons in group
+      // Individual buttons in group appear in the same category, right after the header
       qbGroup.Buttons.forEach((qb) => {
-        const presetQb = createPresetFromButton(buttonCategory, qb, sibIcons)
+        const presetQb = createPresetFromButton(collectionCategory, qb, sibIcons)
         if (presetQb != null) {
           presets[`preset_qb_${qb.Id}`] = presetQb
         }
